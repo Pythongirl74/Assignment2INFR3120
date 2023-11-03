ce32bacba57d1f5a32c22f590afa4c1da117c44b
Jessie Emeshili <jessie.emeshili@ontariotechu.net>
2023-11-02 22:32:28 -0400

    First Commit

---
+++ public/Scripts/pubapp.js
@@ -0,0 +1,10 @@
+/* SPA --> Single Page Application */
+/* Client side routing */
+// IIIFE --> Immediately Invoked Function Expression
+(function () {
+    function Start()
+    {
+        console.log("App Started...");
+    }
+    window.addEventListener("load", Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us' });
+});
+
+module.exports = router;
+/*
+MVC --> Model View Controller
+model --> to connect our logic to the database
+view --> pages that we see in the browser
+controller --> the logic behind our routes
+*/
